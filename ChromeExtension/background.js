ac5bc6f4839e682034ed5c43cddc29b3460d05be
Anders Wind <wind@airtame.com>
2015-09-17 09:44:00 +0200

    Added version 0.1 to github.

---
+++ ChromeExtension/background.js
@@ -0,0 +1,54 @@
+var openString = "";
+
+
+
+function getIsAnalogOpen(callback, errorCallback) {
+  var x = new XMLHttpRequest();
+  x.open('GET', "http://cafeanalog.dk/REST/");
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  x.responseType = 'json';
+  x.onload = function() {
+    var response = x.response;
+    if (!response ) {
+        errorCallback('No response from CafeAnalog.com!');
+        return;
+    }
+    var isOpen = response.open;
+    
+    callback(isOpen);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
+function updateOpenString() {
+  getIsAnalogOpen(function (boolValue){
+    if (boolValue)
+    {
+      openString = "Open";
+    }
+    else{
+      openString = "";
+    }
+  },function(error){
+    openString = "";
+  })
+}
+
+function updateIcon() {
+	chrome.browserAction.setIcon({path:"Assets/icon" + openString + "38.png"});
+}
+
+function updateAll() 
+{
+  updateOpenString();
+  updateIcon();
+}
+
+setInterval(function () {
+  updateAll();
+}, 1800000) // every 30 minutes
+
+updateAll();
---
+++ ChromeExtension/popup.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+function getIsAnalogOpen(callback, errorCallback) {
+  var x = new XMLHttpRequest();
+  x.open('GET', "http://cafeanalog.dk/REST/");
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  x.responseType = 'json';
+  console.log("test 1");
+  x.onload = function() {
+    var response = x.response;
+    if (!response ) {
+        errorCallback('No response from CafeAnalog.com!');
+        return;
+    }
+    console.log(response.open);
+    var isOpen = response.open;
+    console.log(isOpen);
+    
+    callback(isOpen);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+function boolToText(isOpenBool) {
+  var toReturn = "";
+  if (isOpenBool)
+  {
+    toReturn = "ÅPEN";
+    setIcon("Open");
+  } 
+  else
+  {
+    toReturn = "CLØSED";
+    setIcon("");
+  }
+  return toReturn;
+}
+
+function setIcon(openString) {
+  chrome.browserAction.setIcon({path:"Assets/icon"+openString+"38.png"});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderStatus('Cafe Analog is...');
+  getIsAnalogOpen(function (boolValue){
+    console.log(boolValue);
+    var result = boolToText(boolValue);
+    console.log(result);
+    renderStatus('Cafe Analog is ' + result);
+  }, function (error) {
+    renderStatus('Something went wrong: ' + error);
+  });
+});
